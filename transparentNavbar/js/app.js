96c111cf145430fb971c4728082741b6c44430ea
Conrado1212 <conrado1212@gmail.com>
2023-09-16 19:15:04 +0200

    change active li

--- transparentNavbar/js/app.js
+++ transparentNavbar/js/app.js
@@ -1,12 +1,21 @@
 
 var menu = document.getElementById("menu");
-console.log(menu);
+// console.log(menu);
 var tab = menu.getElementsByClassName("tab");
-for (var i = 0; i < tab.length; i++) {
-    tab[i].addEventListener("click",function(){
-        var current = document.getElementsByClassName("active");
-        console.log(current);
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+// for (var i = 0; i < tab.length; i++) {
+//     tab[i].addEventListener("click",function(){
+//         var current = document.getElementsByClassName("active");
+//         console.log(current);
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
+
+
+window.addEventListener('load',function(){
+    $('#menu li').click(function () {
+        $('#menu li').removeClass('active');
+
+      $(this).addClass('active');
+    })
+})
