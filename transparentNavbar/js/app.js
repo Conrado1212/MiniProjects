70672b5403c88cf8d167d1e4c3ba3bf978bc5e08
Conrado1212 <conrado1212@gmail.com>
2023-09-18 19:44:39 +0200

    change active li and search bar

--- transparentNavbar/js/app.js
+++ transparentNavbar/js/app.js
@@ -19,3 +19,27 @@ window.addEventListener('load',function(){
       $(this).addClass('active');
     })
 })
+
+
+// $(function() { 
+//   //for bootstrap 3 use 'shown.bs.tab' instead of 'shown' in the next line
+//   $('a[data-toggle="tab"]').on('shown', function (e) {
+//     //save the latest tab; use cookies if you like 'em better:
+//     localStorage.setItem('lastTab', $(e.target).attr('id'));
+//   });
+
+//   //go to the latest tab, if it exists:
+//   var lastTab = localStorage.getItem('lastTab');
+//   if (lastTab) {
+//       $('#'+lastTab).tab('show');
+//   }
+// });
+
+
+// get the actual pathname:
+var path = location.pathname;
+// filter menu items to find one that has anchor tag with matching href:
+$('#menu li').filter(function(){
+    return '/' + $('a', this).attr('href') === path;
+// add class active to the item:
+}).addClass('active');
