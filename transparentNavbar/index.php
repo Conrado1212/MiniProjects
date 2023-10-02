c0d0f3f56f65d06c28f26f7c410b13075d543931
Conrado1212 <conrado1212@gmail.com>
2023-10-02 19:18:07 +0200

    add search icon

--- transparentNavbar/index.php
+++ transparentNavbar/index.php
@@ -36,20 +36,7 @@
   
                     
 
-               <div class="social">
-                <div class="fb">
-                    <a href=" https://www.facebook.com/"><i class="fa-brands fa-square-facebook"></i></a>
-                  </div>
-                  <div class="yt">
-                      <a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a>
-                  </div>
-                  <div class="tw">
-                      <a href="https://www.twitch.tv/"><i class="fa-brands fa-twitch"></i></a>
-                  </div>
-                  <div class="am">
-                     <a href="https://www.amazon.de/"><i class="fa-brands fa-square-instagram"></i></a>
-      
-                  </div>
+                <?php include('part/social.php'); ?>
                   <div style="clear: both"> </div>
                </div>
                 <div class="footer">
