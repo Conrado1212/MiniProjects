c7073e2c02a4b458eb4427b8a9e57c9399ce0a75
Conrado1212 <conrado1212@gmail.com>
2023-10-03 18:47:11 +0200

    add style part

---
+++ transparentNavbar/part/part.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href="css/fontello.css" rel="stylesheet" type="text/css" />
+    <script src="https://kit.fontawesome.com/236e28bd84.js" crossorigin="anonymous"></script>
+    <script src="./js/app.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
