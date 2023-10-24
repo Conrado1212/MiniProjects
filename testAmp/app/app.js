04adf664c33c9953434741f9d674fec902d98fb7
Conrado1212 <conrado1212@gmail.com>
2023-10-24 18:52:50 +0200

    add & example in text area

--- testAmp/app/app.js
+++ testAmp/app/app.js
@@ -1,6 +1,5 @@
 function xd(){
-    var xd = document.getElementById('test').innerHTML
-    console.log(xd);
-//add navigator 
-var x = "<b>Success</b> is a matter of hard work &amp;luck";
+    var xd = document.getElementById('test').value
+   navigator.clipboard.writeText(xd);
+
 }
