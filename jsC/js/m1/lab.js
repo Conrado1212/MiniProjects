895ca6cb7e14d90e94b75a8ef3b713f34e063044
Conrado1212 <conrado1212@gmail.com>
2023-12-11 19:23:02 +0100

    js cheat sheet lab1

--- jsC/js/m1/lab.js
+++ jsC/js/m1/lab.js
@@ -4,3 +4,120 @@
 //   Guernica (Pablo Picasso, 1937), The Kiss (Gustav Klimt, 1907), Girl With a Pearl Earring (Johannes Vermeer, 1665), 
 //   The Birth of Venus (Sandro Botticelli, 1485), Las Meninas (Diego Velázquez, 1656), The Creation of Adam (Michelangelo, 1512).
 // Display all the images in the list in the console (full information: title, artist and date of creation).
+
+
+let images  = [{
+    title: "Mona Lisa",
+    artist  : "Leonardo da Vinci",
+    date  : "1503"
+},
+    {
+         title: "The Last Supper",
+    artist  : "Leonardo da Vinci",
+    date  : "1495"   
+},
+{
+    title: "Starry Night",
+artist  : "Vincent van Gogh",
+date  : "1889"    
+},
+{
+    title: "The Scream",
+artist  : "Edvard Munch",
+date  : "1893"    
+},
+{
+    title: "Guernica",
+artist  : "Pablo Picasso",
+date  : "1937"    
+},
+{
+    title: "The Kiss",
+artist  : "Gustav Klimt",
+date  : "1907"    
+}
+,
+{
+    title: "Girl With a Pearl Earring",
+artist  : "Johannes Vermeer",
+date  : "1665"    
+}
+,
+{
+    title: "The Birth of Venus",
+artist  : "Sandro Botticelli",
+date  : "1485"    
+}
+,
+{
+    title: "Las Meninas",
+artist  : "Diego Velázquez",
+date  : "1656"    
+}
+,
+{
+    title: "The Creation of Adam",
+artist  : "Michelangelo",
+date  : "1512"    
+}
+];
+
+for (var i = 0; i < images.length; i++){
+console.log("Title: "+images[i].title + " artist: " + images[i].artist + " date of creation: " + images[i].date);
+}
+
+
+// let images = [
+//     {
+//         title: 'Mona Lisa',
+//         artist: 'Leonardo da Vinci',
+//         date: 1503
+//     },
+//     {
+//         title: 'The Last Supper',
+//         artist: 'Leonardo da Vinci',
+//         date: 1495
+//     },
+//     {
+//         title: 'The Starry Night',
+//         artist: 'Vincent van Gogh',
+//         date: 1889
+//     },
+//     {
+//         title: 'The Scream',
+//         artist: 'Edvard Munch',
+//         date: 1893
+//     },
+//     {
+//         title: 'Guernica',
+//         artist: 'Pablo Picasso',
+//         date: 1937
+//     },
+//     {
+//         title: 'The Kiss',
+//         artist: 'Gustav Klimt',
+//         date: 1907
+//     },
+//     {
+//         title: 'Girl With a Pearl Earring',
+//         artist: 'Johannes Vermeer',
+//         date: 1665
+//     },
+//     {
+//         title: 'The Birth of Venus',
+//         artist: 'Sandro Botticelli',
+//         date: 1485
+//     },
+//     {
+//         title: 'Las Meninas',
+//         artist: 'Diego Velázquez',
+//         date: 1656
+//     },
+//     {
+//         title: 'Creation of Adam',
+//         artist: 'Michelangelo',
+//         date: 1512
+//     }
+// ];
+// images.forEach(image => {console.log(`${image.title}
+// (${image.artist}, ${image.date})`)});
