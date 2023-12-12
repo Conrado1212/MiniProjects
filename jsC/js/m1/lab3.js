890f63344c63e8308c108f4a845f0aab99529aa1
Conrado1212 <conrado1212@gmail.com>
2023-12-12 19:18:16 +0100

    js cheat sheet labs

---
+++ jsC/js/m1/lab3.js
@@ -0,0 +1,9 @@
+// Create an images object that will be used to store the images. The object should have a list property, which will be an array of image objects and methods:
+
+// contains - which takes as its argument the title of the image and returns true if the image is already placed in the list (otherwise it returns false)
+// add – which takes three arguments (title, artist, and date) and creates a new object based on them and adds it to the list (if it has not already been added)
+// show - which displays all images on the list;
+// clear – which removes all objects from the list.
+// While creating an object, use the Image constructor prepared in the earlier task.
+
+// Test the script by calling the sequence:
---
+++ jsC/js/m1/lab5.js
@@ -0,0 +1,5 @@
+// Write a function, deepComp, that will compare two objects given as arguments (deep comparison). Compare only properties (ignore methods), and consider the possibility of nesting (any number of levels).
+
+// Properties can also be objects and arrays. We are interested in the properties available during the usual enumeration.
+
+// For testing, use the following piece of code:
