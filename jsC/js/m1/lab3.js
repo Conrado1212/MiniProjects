428f48028853764fc2959b5e85ace119129d93d2
Conrado1212 <conrado1212@gmail.com>
2024-01-13 18:56:31 +0100

    js labs3

--- jsC/js/m1/lab3.js
+++ jsC/js/m1/lab3.js
@@ -2,6 +2,7 @@
 let images  = {
     //The object should have a list property
    list:[],
+  
 // contains - which takes as its argument the title of the image and returns true if the image is already placed in the list (otherwise it returns false)
 contains: function(title){
     let val = false;
@@ -14,6 +15,7 @@ contains: function(title){
 }
 return val;
 },
+
 // add – which takes three arguments (title, artist, and date) and creates a new object based on them and adds it to the list (if it has not already been added)
 add: function(title,artist,date){
     if(!this.contains(title)){
@@ -22,28 +24,31 @@ add: function(title,artist,date){
 },
 // show - which displays all images on the list;
 show: function(){
-    for (var i = 0; i < images.list.length; i++){
-        console.log("Title: "+images.list[i].title + " artist: " + images.list[i].artist + " date of creation: " + images.list[i].date);
-        }
+    //  for (var i = 0; i < images.list.length; i++){
+    //      console.log("Title: "+images.list[i].title + " artist: " + images.list[i].artist + " date of creation: " + images.list[i].date);
+    //      }
+    console.log(images.list);
+  //  images.list.forEach(image => {console.log(`${image.list.title} (${image.list.artist}, ${image.list.date})`)});
 },
+
+// clear – which removes all objects from the list.
 clear: function(){
     images.list.splice(0,images.list.length)
 }
 }
-
-
-
-// clear – which removes all objects from the list.
-
-// While creating an object, use the Image constructor prepared in the earlier task.
-// let image = function(title, artist, date) {
+let image = function(title, artist, date) {
   
    
-//     this.title = title;
-//     this.artist = artist;
-//     this.date = date;
+    this.title = title;
+   this.artist = artist;
+     this.date = date;
  
-// };
+ }
+
+
+
+// While creating an object, use the Image constructor prepared in the earlier task.
+
 // Test the script by calling the sequence:
 images.add('Mona Lisa', 'Leonardo da Vinci', 1503);
 images.add('The Last Supper', 'Leonardo da Vinci', 1495);
