58e7712bb968ca473f782b2b42febe48c1421ba7
Conrado1212 <conrado1212@gmail.com>
2024-01-13 19:06:56 +0100

    js labs5

--- jsC/js/m1/lab5.js
+++ jsC/js/m1/lab5.js
@@ -3,3 +3,14 @@
 // Properties can also be objects and arrays. We are interested in the properties available during the usual enumeration.
 
 // For testing, use the following piece of code:
+let a={x:[1,2,3,4,5], y:0, z: {m:'test', n:false}};
+let b={x:[1,2,3,4,5], y:0, z: {m:'test', n:false}};
+let c={x:[1,2,3,4,5,6], y:0, z: {m:'test', n:false}};
+let d={x:[1,2,3,4], y:0, z: {m:'test', n:false}};
+let e={x:[1,2,3,4,5], y:0, z: {m:'test', n:true}};
+let f={x:[1,2,3,4,5], y:-1, z: {m:'test', n:false}};
+console.log(deepComp(a,b)); // -> true
+console.log(deepComp(a,c)); // -> false
+console.log(deepComp(a,d)); // -> false
+console.log(deepComp(a,e)); // -> false
+console.log(deepComp(a,f)); // -> false
