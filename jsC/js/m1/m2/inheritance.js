eb7685fed7efc38061b700fea530d0cc6698a273
Conrado1212 <conrado1212@gmail.com>
2024-01-15 18:31:01 +0100

    js getSet

---
+++ jsC/js/m1/m2/inheritance.js
@@ -0,0 +1,31 @@
+
+class Vehicle { 
+    constructor({id, latitude, longitude}){ 
+        this.id = id; 
+        this.position = {latitude, longitude}; 
+        this.status = "unavailable"; 
+    };
+    set position({latitude, longitude}) { 
+        this.time = Date.now(); 
+        this.longitude = longitude; 
+        this.latitude = latitude; 
+    };
+    get position() { 
+        return { 
+            latitude: this.latitude, 
+            longitude: this.longitude 
+        }; 
+    };
+};
+let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL1024"});
+vehicle.position = {longitude: 18.193121, latitude: 59.378654};
+console.log(vehicle.position);
+
+Class Bus extends Vehicle {
+    constructor({seats}) {
+        this.seats = seats;
+    }
+}
+let bus = new PassengerVehicle({seats: 40}); 
+console.log(bus.seats); // -> 40
+console.log(bus.id); // -> ! undefined
