f4b3c206fc4c2f08825595d453d57022f76c26bd
Conrado1212 <conrado1212@gmail.com>
2024-01-15 18:36:42 +0100

    js inheritance

--- jsC/js/m1/m2/inheritance.js
+++ jsC/js/m1/m2/inheritance.js
@@ -21,11 +21,59 @@ let vehicle = new Vehicle({longitude: 18.213423, latitude: 59.367628, id: "AL102
 vehicle.position = {longitude: 18.193121, latitude: 59.378654};
 console.log(vehicle.position);
 
-Class Bus extends Vehicle {
-    constructor({seats}) {
+class Bus extends Vehicle {
+    constructor({seats, id, latitude, longitude}) {
+        super({id,latitude,longitude})
         this.seats = seats;
     }
 }
-let bus = new PassengerVehicle({seats: 40}); 
+let bus = new Car({seats: 4,longitude: 18.213423, latitude: 59.367628, id: "AL1024"}); 
 console.log(bus.seats); // -> 40
-console.log(bus.id); // -> ! undefined
+console.log(bus.id); // -> "AL1024"
+
+class AlmostEmptyClass { 
+    constructor(sth) { 
+        console.log(sth); 
+    }; 
+    sayHi() { 
+        console.log("Hi!") 
+    }; 
+}; 
+    class ExtendedClass extends AlmostEmptyClass {
+        constructor(name) {
+            super("I’m super ...");
+            this.name = name;
+        };
+    sayHi() { 
+        console.log(`Hi ${this.name}!`); 
+    };
+    newHi() {
+        this.sayHi();
+    }
+    oldHi() {
+        super.sayHi();
+    };
+};
+let obj = new ExtendedClass("Bob"); // -> I’m super ...
+obj.sayHi();    // -> Hi Bob!
+obj.newHi();    // -> Hi Bob!
+obj.oldHi();    // Hi!
+
+let AlmostEmpty = function(sth) { 
+    console.log(sth); 
+    this.sayHi = function() { 
+        console.log("Hi!") 
+    }; 
+};
+
+class ExtendedClass extends AlmostEmpty {
+    constructor(name) {
+        super("I’m super ...");
+        this.name = name;
+    };
+    sayHi() { 
+        console.log(`Hi ${this.name}!`); 
+    };
+};
+let obj = new ExtendedClass("Bob");
+obj.sayHi();    // -> Hi Bob!
