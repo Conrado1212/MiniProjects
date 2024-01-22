b7d9659f8bce10d14460d570bf1abe38ae1afb70
Conrado1212 <conrado1212@gmail.com>
2024-01-22 18:13:41 +0100

    js lab5 class

--- jsC/js/m1/m2/lab5.js
+++ jsC/js/m1/m2/lab5.js
@@ -1,6 +1,18 @@
 //Create an ExtendedTutoring class, inheriting from Tutoring.
-
-//Equip it with one new method: sendMessages(from, to, message). The from argument is the user (student or teacher) who sends the message. The argument to is the list of recipients (user objects).
+class ExtendedTutoring extends Tutoring{
+    constructor(){
+        super();
+    }
+    //Equip it with one new method: sendMessages(from, to, message).
+    sendMessages(from, to = [], message){
+        if(from && to.length){
+            for(let target of to){
+                target.sendMessage(from, message)
+            }
+        }
+    }
+}
+ //The from argument is the user (student or teacher) who sends the message. The argument to is the list of recipients (user objects).
 
 //Test your solution using the following code:
 let tutoring = new ExtendedTutoring();
