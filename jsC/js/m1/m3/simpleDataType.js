c086a9b6b08c502a680a94ed94dbdc00cf4ff36c
Conrado1212 <conrado1212@gmail.com>
2024-01-29 18:35:00 +0100

    js logic constructor number

--- jsC/js/m1/m3/simpleDataType.js
+++ jsC/js/m1/m3/simpleDataType.js
@@ -40,3 +40,30 @@ console.log(x + y); // -> 0.8999999999999999
 console.log((x + y).toFixed(1));    // -> 0.9
 console.log(x / 0);      // -> Infinity
 console.log(x / "abc");  // -> NaN
+
+let nrStr1 = (11).toString();
+let nrStr2 = (11).toString(16);
+console.log(`nrStr1 : ${typeof nrStr1} : ${nrStr1}`); // -> nrStr1 : string : 11
+console.log(`nrStr2 : ${typeof nrStr2} : ${nrStr2}`); // -> nrStr1 : string : b
+
+let numberObj1 = new Number;            // -> 0
+let numberObj2 = new Number(100);       // -> 100
+let numberObj3 = new Number("200");     // -> 200
+let numberObj4 = new Number("abcd");    // -> NaN
+let numberObj5 = new Number(9e10000);   // -> Infinity
+console.log(`numberObj1 : ${typeof numberObj1} : ${ numberObj1.valueOf()}`); // -> numberObj1 : object : 0
+
+
+let nr = Number("100");
+console.log(`nr : ${typeof nr} : ${ nr}`); // -> nr : number : 100
+
+
+let nr2 = Number("200")
+console.log(`ne2 : ${typeof nr2} : ${ nr2}`); // -> nr2 : number : 200
+
+
+let nr3 = Number("abcd")
+console.log(`ne3 : ${typeof nr3} : ${ nr3}`); // -> nr3 : number : abcd
+
+let nr4 = Number("9e10000")
+console.log(`nr4 : ${typeof nr5} : ${ nr3}`); // -> nr4 : number : 9e10000
