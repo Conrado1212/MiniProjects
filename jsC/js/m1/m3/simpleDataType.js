e718d3c7d16632add36c22fc9f9cf752f53186bf
Conrado1212 <conrado1212@gmail.com>
2024-01-29 18:20:31 +0100

    js simpleData

---
+++ jsC/js/m1/m3/simpleDataType.js
@@ -0,0 +1,12 @@
+let strObj = new String("Do bats eat cats?");
+console.log(typeof(strObj)); //-> object
+console.log(strObj.length); // -> 17
+let words = strObj.split(" ");
+console.log(words[0]); // -> Do
+
+
+let str = "Do bats eat cats?";
+console.log(typeof(str)); //-> string
+console.log(str.length); // -> 17
+let words2 = str.split(" ");
+console.log(words2[0]); // -> Do
