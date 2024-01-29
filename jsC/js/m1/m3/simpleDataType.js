7201190a01e6eb4ac4ae24539810edbd26e867b4
Conrado1212 <conrado1212@gmail.com>
2024-01-29 18:28:28 +0100

    js logic

--- jsC/js/m1/m3/simpleDataType.js
+++ jsC/js/m1/m3/simpleDataType.js
@@ -25,3 +25,18 @@ let str2 = bool1.toString();
 let bool3 = bool1.valueOf();
 console.log(`str1 : ${typeof str2} : ${str2}`); // -> str2 : string : false
 console.log(`bool2 : ${typeof bool3} : ${ bool3}`); // -> bool3 : boolean : false
+
+let a = 10; // decimal - default
+let b = 0x10;   // hexadecimal
+let c = 0o10; // octal
+let d = 0b10; // binary
+console.log(a); // -> 10
+console.log(b); // -> 16
+console.log(c); // -> 8
+console.log(d); // -> 2
+let x = 0.3;
+let y = 0.6;
+console.log(x + y); // -> 0.8999999999999999
+console.log((x + y).toFixed(1));    // -> 0.9
+console.log(x / 0);      // -> Infinity
+console.log(x / "abc");  // -> NaN
